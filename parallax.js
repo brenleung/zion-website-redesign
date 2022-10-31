34327e4ebff1590390657eab8f8fc9dd5b822164
Brendan <102711163+brenleung@users.noreply.github.com>
2022-10-31 13:01:15 -0700

    added second part of homepage

--- parallax.js
+++
@@ -1,6 +0,0 @@
-const fronttext = document.getElementById("fronttext");
-
-window.addEventListener("scroll", function() {
-    let offset = window.pageYOffset;
-    fronttext.style.backgroundPositionY = offset * 2 + "px";
-})
